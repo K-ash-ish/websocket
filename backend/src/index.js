353dc38dd504de1e44896df7e554845acd39adf2
K-ash-ish <kashishsondhiya@gmail.com>
2024-01-31 14:05:44 +0530

    initialized express server in the app with some middlewares

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,21 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
 dotenv.config({
   path: "./env",
 });
-connectDB();
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Server error: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, (req, res) => {
+      console.log("Server started at PORT: ", process.env.PORT);
+    });
+  })
+  .catch((error) => console.log("MongoDb connecion failed: ", error));
