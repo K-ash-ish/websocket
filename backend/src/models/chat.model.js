9625e06c61d5ee6c67a3966281ea03ec04254d48
K-ash-ish <kashishsondhiya@gmail.com>
2024-02-02 18:23:35 +0530

    added group model

--- backend/src/models/chat.model.js
+++
@@ -1,5 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-
-const chatSchema = new Schema({});
-
-export const Chat = mongoose.model("Chat", chatSchema);
--- backend/src/models/group.model.js
+++ backend/src/models/group.model.js
@@ -1,5 +1,25 @@
 import mongoose, { Schema } from "mongoose";
 
-const groupSchema = new Schema({});
+const groupSchema = new Schema(
+  {
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: { type: String },
+    bio: { type: String, maxLength: [60, "Can be only 60 characters long!"] },
+    admins: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    publc: { type: Boolean, default: true },
+    members: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  },
+  { timestamps: true }
+);
 
 export const Group = mongoose.model("Group", groupSchema);
