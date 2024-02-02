10dbea3c368d1736f3e778c9efe269053c0c7bec
K-ash-ish <kashishsondhiya@gmail.com>
2024-02-02 18:24:11 +0530

    added bcrypt with mongoose methods and middleware

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -35,13 +36,18 @@ const userSchema = new Schema(
       type: String,
       maxLength: [60, "Can be only 60 characters long!"],
     },
-    chats: [
+    contacts: [
       {
         type: Schema.Types.ObjectId,
-        ref: "Chat",
+        ref: "User",
       },
     ],
-    groups: [{ type: Schema.Types.ObjectId, ref: "Group" }],
+    groups: [
+      {
+        group: { type: Schema.Types.ObjectId, ref: "Group" },
+        isAdmin: { type: Boolean, default: false },
+      },
+    ],
     refreshToken: {
       type: String,
     },
@@ -50,5 +56,34 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateAccessToken = function () {
+  jwt.sign({ _id: this._id }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+  });
+};
 
 export const User = mongoose.model("User", userSchema);
